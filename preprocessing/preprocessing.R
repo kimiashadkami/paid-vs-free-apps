1938a1c1f54b37f03bcf11200305a0a2fcd5be8e
Kimia Shadkami <60762302+kimiashadkami@users.noreply.github.com>
2021-10-29 21:47:31 -0400

    Update preprocessing.R

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -8,6 +8,9 @@ dataset = read.csv("Google-Playstore.csv", na.strings = c("", "NA"), header=TRUE
 #removing the missing data
 clean_dataset <- na.omit(dataset)
 
+#removing the duplicates
+clean_dataset <- clean_dataset[!duplicated(clean_dataset), ]
+
 #saving the data
 #please put in your own directory
 write.csv(clean_dataset, file="D:/DDSE project/data/clean_dataset.csv")
