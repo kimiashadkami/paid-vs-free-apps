f1c751c77daea6cf603360715c16e1176d643466
Kimia Shadkami <60762302+kimiashadkami@users.noreply.github.com>
2021-10-29 21:07:14 -0400

    Update and rename missing_data.R to preprocessing.R

---
+++ preprocessing/preprocessing.R
@@ -0,0 +1,14 @@
+#get data
+getwd()
+#please put in your own directory
+setwd("D:/DDSE project/data/")
+getwd()
+dataset = read.csv("Google-Playstore.csv", na.strings = c("", "NA"), header=TRUE)
+
+#removing the null
+clean_dataset <- na.omit(dataset)
+
+#saving the data
+#please put in your own directory
+write.csv(clean_dataset, file="D:/DDSE project/data/clean_dataset.csv")
+saveRDS(clean_dataset, file="D:/DDSE project/data/clean_dataset_rds.rds")
