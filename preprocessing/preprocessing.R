3d0536a4db99f691e3687a9d4bdecfb52a362878
Kimia Shadkami <60762302+kimiashadkami@users.noreply.github.com>
2021-11-03 11:05:06 -0400

    Update preprocessing.R

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -4,6 +4,9 @@ setwd("D:/DDSE project/data/")
 getwd()
 dataset = read.csv("Google-Playstore.csv", na.strings = c("", "NA"), header=TRUE)
 
+#for later
+options(scipen = 999999999)
+
 #removing the missing data
 clean_dataset <- na.omit(dataset)
 
@@ -51,66 +54,76 @@ length(free_outliers_minandroid)
 paid_outliers_minandroid <- boxplot.stats(as.integer(paid_high_rated$Minimum.Android))$out
 length(paid_outliers_minandroid)
 
-no_outiers_free_high_rated <- free_high_rated[!(free_high_rated$Rating %in% c(free_outliers_rating) | 
+#no outliers
+no_outliers_free_high_rated <- free_high_rated[!(free_high_rated$Rating %in% c(free_outliers_rating) | 
                                                   free_high_rated$Rating.Count %in% c(free_outliers_ratingcount) |
                                                   as.integer(free_high_rated$Minimum.Android) %in% c(free_outliers_minandroid)), ]
 nrow(free_high_rated)
-nrow(no_outiers_free_high_rated)
+nrow(no_outliers_free_high_rated)
 
-#no outliers
-no_outiers_paid_high_rated <- paid_high_rated[!(paid_high_rated$Rating %in% c(paid_outliers_rating) | 
+no_outliers_paid_high_rated <- paid_high_rated[!(paid_high_rated$Rating %in% c(paid_outliers_rating) | 
                                                   paid_high_rated$Rating.Count %in% c(paid_outliers_ratingcount) |
                                                   as.integer(paid_high_rated$Minimum.Android) %in% c(paid_outliers_minandroid)), ]
 nrow(paid_high_rated)
-nrow(no_outiers_paid_high_rated)
-
+nrow(no_outliers_paid_high_rated)
 
 ##installs, removing the + and , sign
-#free_high_rated
-installs1 <- which(colnames(free_high_rated)=="Installs")
-free_high_rated$Installs <- as.character(free_high_rated$Installs)
-for(i in 1:nrow(free_high_rated)){
-  free_high_rated[i,installs1] <- as.factor(substr(free_high_rated[i,installs1], 1, nchar(as.character(free_high_rated[i,installs1]))-1))
-  free_high_rated[i, installs1] <- as.numeric(gsub("\\,", "", free_high_rated[i, installs1]))  
+preprocessing_installs <- function(dt){
+  installs <- which(colnames(dt)=="Installs")
+  dt$Installs <- as.character(dt$Installs)
+  for(i in 1:nrow(dt)){
+    dt[i,installs] <- substr(dt[i,installs], 1, nchar(as.character(dt[i,installs]))-1)
+  }
+  dt$Installs <- as.numeric(gsub("\\,", "", dt$Installs))  
+  dt$Installs <- as.factor(dt$Installs)
 }
-free_high_rated$Installs <- as.factor(free_high_rated$Installs)
 
-#no_outiers_free_high_rated
-installs2 <- which(colnames(no_outiers_free_high_rated)=="Installs")
-no_outiers_free_high_rated$Installs <- as.character(no_outiers_free_high_rated$Installs)
-for(i in 1:nrow(no_outiers_free_high_rated)){
-  no_outiers_free_high_rated[i,installs2] <- as.factor(substr(no_outiers_free_high_rated[i,installs2]
-                                                              , 1, nchar(as.character(no_outiers_free_high_rated[i,installs2]))-1))
-  no_outiers_free_high_rated[i, installs2] <- as.numeric(gsub("\\,", "", no_outiers_free_high_rated[i, installs2]))  
-}
-no_outiers_free_high_rated$Installs <- as.factor(no_outiers_free_high_rated$Installs)
+preprocessing_installs(free_high_rated)
+preprocessing_installs(paid_high_rated)
+preprocessing_installs(no_outliers_free_high_rated)
+preprocessing_installs(no_outliers_paid_high_rated)
 
-#paid_high_rated
-installs3 <- which(colnames(paid_high_rated)=="Installs")
-paid_high_rated$Installs <- as.character(paid_high_rated$Installs)
-for(i in 1:nrow(paid_high_rated)){
-  paid_high_rated[i,installs3] <- as.factor(substr(paid_high_rated[i,installs3], 1, nchar(as.character(paid_high_rated[i,installs3]))-1))
+##size, turning M to 1000000 and K to 1000 and varries with device to -1
+preprocessing_size <- function(dt){
+  dt <- dt[(dt$Size != "Varies with device"), ]
+  #removing commas
+  dt$Size <- as.character(gsub("\\,", "", dt$Size))
+  size <- which(colnames(dt)=="Size")
+  for(i in 1:nrow(dt)){
+    last_char <- strsplit(dt[i,size], '')[[1]][nchar(dt[i,size])]
+    if(last_char == "M" || last_char =="m"){
+      temp <- dt[i,size]
+      temp <- as.numeric(substr(temp, 1, nchar(temp)-1))
+      dt[i,size] <- temp*1000000
+    }
+    else if(last_char == "K" || last_char == "k"){
+      temp <- dt[i,size]
+      temp <- as.numeric(substr(temp, 1, nchar(temp)-1))
+      dt[i,size] <- temp*1000
+    }
+    else if(last_char == "G" || last_char == "g"){
+      print("H")
+      dt[i, size] <- -1
+    }
+  }
+  dt <- subset(dt, Size!=-1)
+  dt$Size <- as.numeric(dt$Size)
   
-  paid_high_rated[i, installs3] <- as.numeric(gsub("\\,", "", paid_high_rated[i, installs3]))  
 }
-paid_high_rated$Installs <- as.factor(paid_high_rated$Installs)
 
-#no_outiers_paid_high_rated
-installs4 <- which(colnames(no_outiers_paid_high_rated)=="Installs")
-no_outiers_paid_high_rated$Installs <- as.character(no_outiers_paid_high_rated$Installs)
-for(i in 1:nrow(no_outiers_paid_high_rated)){
-  no_outiers_paid_high_rated[i,installs4] <- substr(no_outiers_paid_high_rated[i,installs4]
-                                                              , 1, nchar(as.character(no_outiers_paid_high_rated[i,installs4]))-1)
-  no_outiers_paid_high_rated[i, installs4] <- as.numeric(gsub("\\,", "", no_outiers_paid_high_rated[i, installs4]))  
-}
-no_outiers_paid_high_rated$Installs <- as.factor(no_outiers_paid_high_rated$Installs)
+preprocessing_size(free_high_rated)
+preprocessing_size(paid_high_rated)
+preprocessing_size(no_outliers_free_high_rated)
+preprocessing_size(no_outliers_paid_high_rated)
+
+
 
 #saving them
 write.csv(free_high_rated, file="D:/DDSE project/data/free_high_rated.csv")
 saveRDS(free_high_rated, file="D:/DDSE project/data/free_high_rated.rds")
 
-write.csv(no_outiers_free_high_rated, file="D:/DDSE project/data/no_outiers_free_high_rated.csv")
-saveRDS(no_outiers_free_high_rated, file="D:/DDSE project/data/no_outiers_free_high_rated.rds")
+write.csv(no_outliers_free_high_rated, file="D:/DDSE project/data/no_outliers_free_high_rated.csv")
+saveRDS(no_outliers_free_high_rated, file="D:/DDSE project/data/no_outliers_free_high_rated.rds")
 
-write.csv(no_outiers_paid_high_rated, file="D:/DDSE project/data/no_outiers_paid_high_rated.csv")
-saveRDS(no_outiers_paid_high_rated, file="D:/DDSE project/data/no_outiers_paid_high_rated.rds")
+write.csv(no_outliers_paid_high_rated, file="D:/DDSE project/data/no_outliers_paid_high_rated.csv")
+saveRDS(no_outliers_paid_high_rated, file="D:/DDSE project/data/no_outliers_paid_high_rated.rds")
