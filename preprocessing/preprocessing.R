cd620a16795f084bb8a5e50be7f4024461a67f8e
Kimia Shadkami <60762302+kimiashadkami@users.noreply.github.com>
2021-11-02 23:46:39 -0400

    Update preprocessing.R

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -57,12 +57,53 @@ no_outiers_free_high_rated <- free_high_rated[!(free_high_rated$Rating %in% c(fr
 nrow(free_high_rated)
 nrow(no_outiers_free_high_rated)
 
+#no outliers
 no_outiers_paid_high_rated <- paid_high_rated[!(paid_high_rated$Rating %in% c(paid_outliers_rating) | 
                                                   paid_high_rated$Rating.Count %in% c(paid_outliers_ratingcount) |
                                                   as.integer(paid_high_rated$Minimum.Android) %in% c(paid_outliers_minandroid)), ]
 nrow(paid_high_rated)
 nrow(no_outiers_paid_high_rated)
 
+
+##installs, removing the + and , sign
+#free_high_rated
+installs1 <- which(colnames(free_high_rated)=="Installs")
+free_high_rated$Installs <- as.character(free_high_rated$Installs)
+for(i in 1:nrow(free_high_rated)){
+  free_high_rated[i,installs1] <- as.factor(substr(free_high_rated[i,installs1], 1, nchar(as.character(free_high_rated[i,installs1]))-1))
+  free_high_rated[i, installs1] <- as.numeric(gsub("\\,", "", free_high_rated[i, installs1]))  
+}
+free_high_rated$Installs <- as.factor(free_high_rated$Installs)
+
+#no_outiers_free_high_rated
+installs2 <- which(colnames(no_outiers_free_high_rated)=="Installs")
+no_outiers_free_high_rated$Installs <- as.character(no_outiers_free_high_rated$Installs)
+for(i in 1:nrow(no_outiers_free_high_rated)){
+  no_outiers_free_high_rated[i,installs2] <- as.factor(substr(no_outiers_free_high_rated[i,installs2]
+                                                              , 1, nchar(as.character(no_outiers_free_high_rated[i,installs2]))-1))
+  no_outiers_free_high_rated[i, installs2] <- as.numeric(gsub("\\,", "", no_outiers_free_high_rated[i, installs2]))  
+}
+no_outiers_free_high_rated$Installs <- as.factor(no_outiers_free_high_rated$Installs)
+
+#paid_high_rated
+installs3 <- which(colnames(paid_high_rated)=="Installs")
+paid_high_rated$Installs <- as.character(paid_high_rated$Installs)
+for(i in 1:nrow(paid_high_rated)){
+  paid_high_rated[i,installs3] <- as.factor(substr(paid_high_rated[i,installs3], 1, nchar(as.character(paid_high_rated[i,installs3]))-1))
+  
+  paid_high_rated[i, installs3] <- as.numeric(gsub("\\,", "", paid_high_rated[i, installs3]))  
+}
+paid_high_rated$Installs <- as.factor(paid_high_rated$Installs)
+
+#no_outiers_paid_high_rated
+installs4 <- which(colnames(no_outiers_paid_high_rated)=="Installs")
+no_outiers_paid_high_rated$Installs <- as.character(no_outiers_paid_high_rated$Installs)
+for(i in 1:nrow(no_outiers_paid_high_rated)){
+  no_outiers_paid_high_rated[i,installs4] <- substr(no_outiers_paid_high_rated[i,installs4]
+                                                              , 1, nchar(as.character(no_outiers_paid_high_rated[i,installs4]))-1)
+  no_outiers_paid_high_rated[i, installs4] <- as.numeric(gsub("\\,", "", no_outiers_paid_high_rated[i, installs4]))  
+}
+no_outiers_paid_high_rated$Installs <- as.factor(no_outiers_paid_high_rated$Installs)
 
 #saving them
 write.csv(free_high_rated, file="D:/DDSE project/data/free_high_rated.csv")
