c384ce0d48e156cffffc5ce19e7681974f66d975
KimiaShadkami <shadkamk@myumanitoba.ca>
2021-11-17 21:51:33 -0500

    update Main.java for better path name

--- frequent patterns/src/main/java/io/github/kimiashadkami/dataprep/Main.java
+++ frequent patterns/src/main/java/io/github/kimiashadkami/dataprep/Main.java
@@ -4,34 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String str_xlsx1 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/free_high_rated_spmf.xlsx";
-        String str_txt1 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/free_high_rated_spmf.txt";
+        String const_path = "D:/eclipse/workspace/paid-vs-free-apps/frequent patterns/src/main/resources";
 
-        String str_xlsx2 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/paid_high_rated_spmf.xlsx";
-        String str_txt2 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/paid_high_rated_spmf.txt";
+        String str_xlsx1 = const_path + "/free_high_rated_spmf.xlsx";
+        String str_txt1 = const_path + "/free_high_rated_spmf.txt";
 
-        String str_xlsx3 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/no_outliers_free_high_rated_spmf.xlsx";
-        String str_txt3 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/no_outliers_free_high_rated_spmf.txt";
+        String str_xlsx2 = const_path + "/paid_high_rated_spmf.xlsx";
+        String str_txt2 = const_path + "/paid_high_rated_spmf.txt";
 
-        String str_xlsx4 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/no_outliers_paid_high_rated_spmf.xlsx";
-        String str_txt4 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/no_outliers_paid_high_rated_spmf.txt";
+        String str_xlsx3 = const_path + "/no_outliers_free_high_rated_spmf.xlsx";
+        String str_txt3 = const_path + "/no_outliers_free_high_rated_spmf.txt";
 
-        String mis1 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/free_high_rated_MIS.txt";
-        String mis2 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/paid_high_rated_MIS.txt";
-        String mis3 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/no_outliers_free_high_rated_MIS.txt";
-        String mis4 = "D:/eclipse/workspace/paid-vs-free-apps/"
-                + "frequent patterns (data prep + SPMF + post processing)/src/main/resources/no_outliers_paid_high_rated_MIS.txt";
+        String str_xlsx4 = const_path + "/no_outliers_paid_high_rated_spmf.xlsx";
+        String str_txt4 = const_path + "/no_outliers_paid_high_rated_spmf.txt";
+
+        String mis1 = const_path + "/free_high_rated_MIS.txt";
+        String mis2 = const_path + "/paid_high_rated_MIS.txt";
+        String mis3 = const_path + "/no_outliers_free_high_rated_MIS.txt";
+        String mis4 = const_path + "/no_outliers_paid_high_rated_MIS.txt";
 
         Prep prep = new Prep();
         prep.prepData(str_xlsx1, str_txt1);
