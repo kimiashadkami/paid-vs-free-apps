4bd0601cc2c67b56dea6ebe25b6ea03f80026fdd
KimiaShadkami <shadkamk@myumanitoba.ca>
2021-12-02 11:36:44 -0500

    update for visualization ui

--- visualizing/ui/static/js/vis.js
+++ visualizing/ui/static/js/vis.js
@@ -1,4 +1,18 @@
-function visualize(data, str){
+function visualize_pattern(data, str){
+
+    const patterns_vis = []
+    for(let i = 0; i < data.length; i++){
+        patterns_vis.push({
+            "name" : data[i]["pattern"],
+            "value" : data[i]["frequency"]/data[i]["total"]
+        });
+    }
+
+
+}
+
+function visualize_category(data, str){
+    
     //calculating the quanity for each category
     const temp_data = new Map();
     for(let i = 51; i < 99; i++){
@@ -31,7 +45,6 @@ function visualize(data, str){
             "name" : category_names[cat],
             "value" : temp_data.get(i)/sum
         });
-        sum += temp_data.get(i);
         i++;
     }
 
@@ -44,111 +57,14 @@ function visualize(data, str){
 
     //top categories out of the 48 ones
     const vis_data_top = [];
-    for(let i = 0; i < 20; i++){
+    for(let i = 0; i < 10; i++){
         vis_data_top.push({
             "name": vis_data[i].name,
             "value": vis_data[i].value,
         });
     }
-    console.log("hi")
-    console.log(vis_data)
-    console.log(vis_data_top)
+
     //visualizing
-    //bar plot
-    // Copyright 2021 Observable, Inc.
-    // Released under the ISC license.
-    // https://observablehq.com/@d3/bar-chart
-    function BarChart(data, strname, {
-        x = (d, i) => i, // given d in data, returns the (ordinal) x-value
-        y = d => d, // given d in data, returns the (quantitative) y-value
-        title, // given d in data, returns the title text
-        marginTop = 20, // the top margin, in pixels
-        marginRight = 0, // the right margin, in pixels
-        marginBottom = 30, // the bottom margin, in pixels
-        marginLeft = 40, // the left margin, in pixels
-        width = 640, // the outer width of the chart, in pixels
-        height = 400, // the outer height of the chart, in pixels
-        xDomain, // an array of (ordinal) x-values
-        xRange = [marginLeft, width - marginRight], // [left, right]
-        yType = d3.scaleLinear, // y-scale type
-        yDomain, // [ymin, ymax]
-        yRange = [height - marginBottom, marginTop], // [bottom, top]
-        xPadding = 0.1, // amount of x-range to reserve to separate bars
-        yFormat, // a format specifier string for the y-axis
-        yLabel, // a label for the y-axis
-        color = "currentColor" // bar fill color
-        } = {}) {
-        // Compute values.
-        const X = d3.map(data, x);
-        const Y = d3.map(data, y);
-
-        // Compute default domains, and unique the x-domain.
-        if (xDomain === undefined) xDomain = X;
-        if (yDomain === undefined) yDomain = [0, d3.max(Y)];
-        xDomain = new d3.InternSet(xDomain);
-
-        // Omit any data not present in the x-domain.
-        const I = d3.range(X.length).filter(i => xDomain.has(X[i]));
-
-        // Construct scales, axes, and formats.
-        const xScale = d3.scaleBand(xDomain, xRange).padding(xPadding);
-        const yScale = yType(yDomain, yRange);
-        const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
-        const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
-
-        // Compute titles.
-        if (title === undefined) {
-            const formatValue = yScale.tickFormat(100, yFormat);
-            title = i => `${X[i]}\n${formatValue(Y[i])}`;
-        } else {
-            const O = d3.map(data, d => d);
-            const T = title;
-            title = i => T(O[i], i, data);
-        }
-
-        const container = d3.select("#"+strname)
-        
-        const svg = d3.select("#svg-"+strname)
-            .attr("id", strname)
-            .attr("width", width)
-            .attr("height", height)
-            .attr("viewBox", [0, 0, width, height])
-            .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-        svg.append("g")
-            .attr("transform", `translate(${marginLeft},0)`)
-            .call(yAxis)
-            .call(g => g.select(".domain").remove())
-            .call(g => g.selectAll(".tick line").clone()
-                .attr("x2", width - marginLeft - marginRight)
-                .attr("stroke-opacity", 0.1))
-            .call(g => g.append("text")
-                .attr("x", -marginLeft)
-                .attr("y", 10)
-                .attr("fill", "currentColor")
-                .attr("text-anchor", "start")
-                .text(yLabel));
-
-        const bar = svg.append("g")
-            .attr("fill", color)
-            .selectAll("rect")
-            .data(I)
-            .join("rect")
-            .attr("x", i => xScale(X[i]))
-            .attr("y", i => yScale(Y[i]))
-            .attr("height", i => yScale(0) - yScale(Y[i]))
-            .attr("width", xScale.bandwidth());
-
-        if (title) bar.append("title")
-            .text(title);
-
-        svg.append("g")
-            .attr("transform", `translate(0,${height - marginBottom})`)
-            .call(xAxis);
-
-        return svg.node();
-    }
-
     //generate cards
     const container = d3.select(".my-row")
     container.append("div")
@@ -166,20 +82,10 @@ function visualize(data, str){
         .append("svg")
         .attr("id", "svg-"+str)
 
-    /*BarChart(vis_data_top, str, {
-        x: d => d.name,
-        y: d => d.value,
-        xDomain: d3.groupSort(vis_data_top, ([d]) => -d.value, d => d.name), // sort by descending frequency
-        yFormat: "%",
-        yLabel: "↑ Frequency",
-        color: "steelblue",
-        width: 2000,
-        height: 700
-    })*/
-
-    //animation
-
-    function BarChart3(data, strname, {
+    // Copyright 2021 Observable, Inc.
+    // Released under the ISC license.
+    // https://observablehq.com/@d3/sortable-bar-chart
+    function BarChart(data, strname, {
         x = (d, i) => i, // given d in data, returns the (ordinal) x-value
         y = d => d, // given d in data, returns the (quantitative) y-value
         marginTop = 20, // the top margin, in pixels
@@ -343,17 +249,13 @@ function visualize(data, str){
         });
       }
 
-      chart = BarChart3(vis_data_top, str, {
+      chart = BarChart(vis_data_top, str, {
         x: d => d.name,
         y: d => d.value,
         yFormat: "%",
         yLabel: "↑ Frequency",
         color: "steelblue",
-        width: 2000,
-        height: 700,
-        duration: 750
+        width: 1000,
+        height: 700
     })
-
-    update = undefined
-    update = chart.update(d3.sort(vis_data_top, order))
 }
